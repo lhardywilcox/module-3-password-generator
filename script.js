ff1e7f6a610d4ec9cd6cd2c0410b970b483deb67
LHardyWilcox <motacycaryda@mac.com>
2023-01-08 20:48:54 -0800

    code updates

--- script.js
+++ script.js
@@ -1,24 +1,88 @@
-/*// Assignment Code
+
+//Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+
+function generatePassword(userUpper, userLower, userNum) {
+  var lowCase = 'abcdefg';
+  var upCase = 'ABCDEFG';
+  var numChar = '123456789';
+
+  var userLength = prompt("Please enter the length of your password.", "8 to 128 characters");
+  passLength = parseFloat(userLength);
+  console.log(passLength);
+  
+  var userUpper = prompt("Would you like to use upper case letters?", "Y or N");
+  userUpper = userUpper.toLowerCase();
+  
+  var userLower = prompt("Would you like to use lower case letters?", "Y or N");
+  userLower = userLower.toLowerCase();
+  
+  var userNum = prompt ("Would you like to use numeric characters?", "Y or N");
+  userNum = userNum.toLowerCase();
+  
+  console.log(userUpper);
+  console.log(userLower);
+  console.log(userNum);
+
+  //Concat strings of password characters based on user input from prompts
+  if (userUpper === 'y' && userLower === 'y' && userNum === 'y') {
+  var passChar = upCase.concat(lowCase, numChar);
+  /*   } else if (userUpper === 'y' && userLower === 'y' && userNum === 'n') {
+  passChar = upCase.concat(lowCase);
+  } else if (userUpper === 'y' && userLower === 'n' && userNum === 'y') {
+  passChar = upCase.concat(numChar);
+  } else if (userUpper === 'n' && userLower === 'y' && userNum === 'y') {
+  passChar = lowCase.concat(numChar);
+  } else if (userUpper === 'y' && userLower === 'n' && userNum === 'n') {
+  passChar = userUpper;
+  } else if (userUpper === 'n' && userLower === 'y' && userNum === 'n') {
+  passChar = userLower;
+  } else if (userUpper === 'n' && userLower === 'n' && userNum === 'y') {
+  passChar = userNum;
+  } else {
+    return; */
+  };
+
+  console.log(passChar); //Logs character set for password. Correct.
+
+  console.log(getPassword(passLength));//Logs random password creation. Correct.
+
+  function getPassword () {
+    var password = "";
+    for (var i, i = 0; i < passLength; i++) {
+      password += passChar.charAt(Math.floor(Math.random() * passChar.length));
+    }
+    return password;
+  } 
+
+  writePassword();
+
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    var p = document.createElement(p);
+    
+    document.textarea.appendChild(p);
+    p.passwordText.value;
+    
+    passwordText.value = password;
+  }
+
+
+
+
+};
+
+
 // Write password to the #password input
-function writePassword() {
+/* function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);*/
-
-let passLength = prompt("Please enter the length of your password.", "8 to 128 characters");
-  if (passLength >= 8 && passLength <= 128) {
-    console.log("Thank you.  Your password will be " + passLength + " characters long.");
-  } else {
-    console.log("Please try again.");
-  };
-
-
-
+writePassword(); */
